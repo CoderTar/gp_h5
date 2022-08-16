1f085071a9bc993bca389100ae09ef7cea0064d0
bbc <bbc@it66.com>
2022-08-16 22:21:37 +0800

    add tabar

---
+++ pages/mine/mine.vue
@@ -0,0 +1,42 @@
+<template>
+	<view>
+		<!-- 1.partOne -->
+		<view class="partOne">
+
+			<!-- 头像和号码 -->
+			<view class="profile_title">
+				<u-avatar size='60'></u-avatar>
+				<text style="font-size: 66rpx;color: white;">19612348970</text>
+			</view>
+			
+			<view class="money_text">
+				
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.partOne {
+		height: 400rpx;
+		background-color: #ff4d4f;
+	}
+	
+	.profile_title{
+		border: 1px solid green;
+		display: flex;
+	}
+</style>
---
+++ pages/quotation/quotation.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		行情
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
